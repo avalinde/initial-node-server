81d5da4f88d3e497f37dd25c7e67cc7982581bd0
unknown <evermorie@gmail.com>
2025-05-03 10:56:00 -0400

    Classwork 05032025 part1

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ const PORT = 3000;
 app.use(cors());
 app.use(morgan("dev"));
 
+//add the path module
+const path = require("node:path");
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
 //combined - show a log that is more comprehensive, dev will show simpler information
 
 //have the app listen at the PORT where a console.log says `Server is listening on ${PORT}. Connection established.`
@@ -28,21 +37,69 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res, next) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
+  res.status(200).json({
+    success: { message: "This route points to the index page" },
+    statusCode: 200,
+  });
 });
 
 app.get("/admin", (req, res, next) => {
-    res.send("This route points to the admin console page")
-} )
+  // res.send("This route points to the admin console page")
+  res.status(200).json({
+    success: { message: "This route points to the admin page" },
+    statusCode: 200,
+  });
+});
 
-app.get("/author", (req, res, next) => {
-    res.send("This route points to the author page")
-} )
+app.get("/authors", (req, res, next) => {
+  //  res.send("This route points to the author page")
+  res.status(200).json({
+    success: { message: "This route points to the author page" },
+    statusCode: 200,
+  });
+});
 
 app.get("/books", (req, res, next) => {
-    res.send("This route points to the books page")
-} )
+  // res.send("This route points to the books page")
+  res.status(200).json({
+    success: { message: "This route points to the books page" },
+    statusCode: 200,
+  });
+});
 
 app.get("/site-routes", (req, res, next) => {
-    res.send("This route points to the site router page")
-} )
+  //res.send("This route points to the site router page")
+  res.status(200).json({
+    success: { message: "This route points to the site router page" },
+    statusCode: 200,
+  });
+});
+
+app.get("/books/create", (req, res, next) => {
+  res.status(200).json({
+    success: { message: "This route points to the Create Book page" },
+    statusCode: 200,
+  });
+});
+
+app.get("/books/:_id", (req, res, next) => {
+  res.status(200).json({
+    success: { message: "This route points to the specific Book via the ID" },
+    statusCode: 200,
+  });
+});
+
+app.get("/authors/create", (req, res, next) => {
+  res.status(200).json({
+    success: { message: "This route points to the Create Author page" },
+    statusCode: 200,
+  });
+});
+
+app.get("/authors/:_id", (req, res, next) => {
+  res.status(200).json({
+    success: { message: "This route points to the specific author via the ID" },
+    statusCode: 200,
+  });
+});
